5e86f8b9f73ddce290ccfd0cb20c1c7347b4e0a8
didiotghima <nurlanuuulubeksultan@gmail.com>
2024-02-28 17:03:32 -0500

    initial commit

--- src/queries/core.py
+++ src/queries/core.py
@@ -1,32 +1,33 @@
 import asyncio
-from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy import text
-from config import settings
 from models import metadata_obj
+from database import async_engine, sync_engine
 
-async_engine = create_async_engine(
-    settings.DATABASE_URL_asyncpg,
-    echo=True,
-)
-
-async def get_123_sync():
-    async with async_engine.connect() as conn:
-        res = await conn.execute(text("SELECT 1,2,3 UNION SELECT 4,5,6"))
-        print(f"{await res.first()=}")
+def get_123_sync():
+    with sync_engine.connect() as conn:
+        res = conn.execute(text("SELECT 1,2,3 UNION SELECT 4,5,6"))
+        print(f"{res.first()=}")
 
 async def get_123_async():
     async with async_engine.connect() as conn:
         res = await conn.execute(text("SELECT 1,2,3 UNION SELECT 4,5,6"))
-        print(f"{await res.first()=}")
+        row = await res.fetchone()
+        print(f"{row=}")
 
 async def create_tables():
     async with async_engine.begin() as conn:
         await conn.run_sync(metadata_obj.create_all)
 
+def drop_tables():
+    with sync_engine.connect() as conn:
+        metadata_obj.drop_all(conn)
+
 async def main():
     await create_tables()
-    await get_123_sync()
     await get_123_async()
+    get_123_sync()
+    drop_tables()
 
+# Проверяем, что код запускается только если файл запущен как скрипт, а не импортирован в другой файл.
 if __name__ == "__main__":
     asyncio.run(main())
